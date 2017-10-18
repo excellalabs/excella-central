ba3efe2e9daf3ca2a9201c7743e0c5a6423e3fb7
Hugh Gardiner <gardinerhugh6@gmail.com>
2017-10-18 00:02:21 -0400

    began initial page additions and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { DirectoryPage } from '../pages/directory/directory';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Directory', component: ListPage }
+      { title: 'Directory', component: DirectoryPage },
+      { title: 'Login', component: LoginPage }
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Nav } from 'ionic-angular';
+
+import { DirectoryPage } from '../../pages/directory/directory';
+import { LoginPage } from '../../pages/login/login';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  rootPage: any = HomePage;
+  pages: Array<{title: string, component: any}>;
+  @ViewChild(Nav) nav: Nav;
+  
   constructor(public navCtrl: NavController) {
+    this.pages = [
+      { title: 'Directory', component: DirectoryPage },
+      { title: 'Login', component: LoginPage }
+    ];
+  }
 
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 
 }
