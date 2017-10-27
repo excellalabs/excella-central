f825341f80912ec3c967ace6ed0411911aca8aa0
Brendan Sawyer <brendan.sawyer@excella.com>
2017-10-26 21:43:41 -0400

    Fixing injection bug preventing Directory screen from rendering

--- src/providers/api.service/api.service.ts
+++ src/providers/api.service/api.service.ts
@@ -2,16 +2,18 @@ import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { User } from '../../models/user/user';
-import { USERS_API_URL } from '../../app/app-config';
+import { ConnectionString, UsersInjectionToken } from '../../app/app-config';
 
 @Injectable()
 export class ApiServiceProvider {
-  constructor(private http: Http, @Inject(USERS_API_URL) private apiUrl) {
+  constructor(
+    private http: Http,
+    @Inject(UsersInjectionToken) private usersApi: ConnectionString) {
   }
 
   async getUsers(): Promise<User[]> {
     let users = new Array<User>();
-    await this.http.get(this.apiUrl).subscribe(data => {
+    await this.http.get(this.usersApi.url).subscribe(data => {
       JSON.parse(data["_body"])["results"].forEach(user => {
         users.push({
           firstName: user.name.first,
