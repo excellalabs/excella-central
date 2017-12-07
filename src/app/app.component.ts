d40b5186211c52e2480f080b0762535b02ebd1d6
Cameron Ivey <cameron.ivey@excella.com>
2017-12-06 20:17:12 -0500

    register page
    presentation, validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,8 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: 'HomePage' },
       { title: 'Directory', component: 'DirectoryPage' },
-      { title: 'Login', component: 'LoginPage' }
+      { title: 'Login', component: 'LoginPage' },
+      { title: 'Register', component: 'RegisterPage'}
     ];
 
   }
