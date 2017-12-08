b36b2303fdafaf8e26231e10adb876524fe60c36
Alex Hoffman <alexhoffman617@gmail.com>
2017-12-08 08:07:50 -0500

    Merge fix

--- src/providers/account.service/account.service.ts
+++ src/providers/account.service/account.service.ts
@@ -8,10 +8,7 @@ import {
 } from '../../app/app-config';
 import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/toPromise';
-<<<<<<< HEAD
 import { HttpParams } from '@angular/common/http';
-=======
->>>>>>> master
 
 @Injectable()
 export class AccountServiceProvider {
