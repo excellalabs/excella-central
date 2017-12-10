5708cd8345492cc748a635936027c8a59a6dccab
samblissard <samblissard@gmail.com>
2017-12-10 18:56:55 -0500

    Checking email before sending email

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../../providers/account.service/account.service';
 @IonicPage()
@@ -10,8 +10,9 @@ import { AccountService } from '../../providers/account.service/account.service'
 export class ResetPasswordPage {
   public resetForm: FormGroup;
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private alertCtrl: AlertController,
     private formBuilder: FormBuilder,
     private accountService: AccountService) {
     this.resetForm = this.formBuilder.group({
@@ -20,7 +21,17 @@ export class ResetPasswordPage {
   }
 
   sendResetEmail() {
-
+    this.accountService.checkAccountExists(this.resetForm.value.email).then(accountExists => {
+      if (accountExists) {
+        // send email
+      } else {
+        const alert = this.alertCtrl.create({
+          title: 'Account for the provided email does not exist.',
+          buttons: ['OK']
+        });
+        alert.present();
+      }
+    })
   }
 
 }
