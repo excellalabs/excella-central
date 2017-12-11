14b54ecfceeaa840462c1a11a7e4bb4beebf76f3
Alex Hoffman <alexhoffman617@gmail.com>
2017-12-11 10:38:23 -0500

    More test fixes

--- src/pages/picture-upload/picture-upload.spec.ts
+++ src/pages/picture-upload/picture-upload.spec.ts
@@ -1,8 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule, NavController, NavParams } from 'ionic-angular';
-import { By } from '@angular/platform-browser';
-import { NavMock, NavParamsMock } from '../../../test-config/mocks-ionic';
+import {
+  NavMock,
+  NavParamsMock,
+  AccountServiceMock,
+  StorageMock,
+  ProfileServiceMock,
+  PictureUploadServiceMock,
+  AlertControllerMock
+} from '../../../test-config/mocks-ionic';
+import { HttpModule } from '@angular/http';
+import { Storage } from '@ionic/storage/es2015/storage';
+import { AccountService } from '../../providers/account.service/account.service';
+import { PictureUploadService } from '../../providers/picture-upload.service/picture-upload.service';
 import { PictureUploadPage } from './picture-upload';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 describe('PictureUploadPage', () => {
   let fixture: ComponentFixture<PictureUploadPage>;
@@ -14,7 +26,11 @@ describe('PictureUploadPage', () => {
         imports: [IonicModule.forRoot(PictureUploadPage)],
         providers: [
           { provide: NavController, useClass: NavMock },
-          { provide: NavParams, useClass: NavParamsMock }
+          { provide: NavParams, useClass: NavParamsMock },
+          { provide: Storage, useClass: StorageMock },
+          { provide: AlertController, useClass: AlertControllerMock },
+          { provide: AccountService, useClass: AccountServiceMock },
+          { provide: PictureUploadService, useClass: PictureUploadServiceMock }
         ]
       });
       fixture = TestBed.createComponent(PictureUploadPage);
