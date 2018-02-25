3626b9f5efc582e4bee53103307b21bd14d8fc46
samblissard <samblissard@gmail.com>
2018-02-25 17:34:51 -0500

    Triggering password reset with form

--- src/pages/reset-password-form/reset-password-form.ts
+++ src/pages/reset-password-form/reset-password-form.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../../providers/account.service/account.service';
 import { PasswordValidator } from '../../validators/passwords';
@@ -20,10 +20,12 @@ export class ResetPasswordFormPage {
   public resetPasswordForm: FormGroup;
   constructor(
     public navCtrl: NavController,
+    private alertCtrl: AlertController,
     public navParams: NavParams,
     private formBuilder: FormBuilder,
     private accountService: AccountService) {
     this.resetPasswordForm = this.formBuilder.group({
+      resetToken: ['', [Validators.required]],
       newPassword: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: [
         '',
@@ -33,7 +35,18 @@ export class ResetPasswordFormPage {
   }
 
   resetPassword() {
-
+    this.accountService.resetPassword(this.resetPasswordForm.value.newPassword, this.resetPasswordForm.value.resetToken)
+      .then(success => {
+        if (success) {
+          this.navCtrl.push('LoginPage');
+        } else {
+          const alert = this.alertCtrl.create({
+            title: 'Access token is invalid',
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+      });
   }
 
   ionViewDidLoad() {
