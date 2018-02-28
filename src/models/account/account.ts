eda06ce5161e5fa3d0ceb31f2858748030f2304f
Brendan Sawyer <brendan.sawyer@excella.com>
2018-02-28 17:43:52 -0500

    Revert "Merge branch 'multi-tenant' into master"
    This reverts commit 46dcb1e3545ec9b571f802703752e9f724d7f9a8, reversing
    changes made to 3dc2eb44d722d6a63513a70eccf3082901f184c7.

--- src/models/account/account.ts
+++ src/models/account/account.ts
@@ -2,13 +2,13 @@ export class Account {
     id?: string;
     email: string;
     password: string;
-    userType: string;
+    isAdmin: boolean;
     emailVerified: boolean;
 
-    constructor(email: string, password: string, userType: string, emailVerified: boolean) {
+    constructor(email: string, password: string, isAdmin: boolean, emailVerified: boolean) {
         this.email = email;
         this.password = password;
-        this.userType = userType;
+        this.isAdmin = isAdmin;
         this.emailVerified = emailVerified;
     }
 }
