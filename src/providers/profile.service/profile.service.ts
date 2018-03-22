2539e10c349d414674d575b58a76d15bc96fa335
Brendan Sawyer <brendan.sawyer@excella.com>
2018-03-21 22:23:43 -0400

    Fix for parsing json

--- src/providers/profile.service/profile.service.ts
+++ src/providers/profile.service/profile.service.ts
@@ -18,7 +18,7 @@ export class ProfileService {
       this.http
         .get(this.profilesApi.url + '/getAllOrderedByFirstName', requestHeaders)
         .subscribe(data => {
-          resolve(data.json());
+          resolve(data.json().profiles);
         });
     });
   }
@@ -38,7 +38,7 @@ export class ProfileService {
           params: requestParams
         })
         .subscribe(data => {
-          resolve(data.json());
+          resolve(data.json().profiles);
         });
     });
   }
@@ -59,7 +59,7 @@ export class ProfileService {
           params: requestParams
         })
         .subscribe(data => {
-          resolve(data.json());
+          resolve(data.json().profiles);
         });
     });
   }
