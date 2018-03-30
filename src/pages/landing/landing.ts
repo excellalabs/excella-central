b3320482774dcae9b97aa81cdbe3e34d23d50cc2
Brendan Sawyer <brendan.sawyer@excella.com>
2018-03-29 21:10:15 -0400

    Workaround for Ionic bug

--- src/pages/landing/landing.ts
+++ src/pages/landing/landing.ts
@@ -34,7 +34,7 @@ export class LandingPage {
   }
 
   checkIfBrowser(): void {
-    // Determine if user is viewing on desktop
-    this.isBrowser = this.platform.is('core') || this.platform.is('mobileweb');
+    // Determine if user is viewing on browser
+    this.isBrowser = document.URL.startsWith('http');
   }
 }
