287a088f6213fd8c055bb95dc1363155058a2f03
Matt Ratliff <matthew.ratliff@gmail.com>
2018-07-25 10:40:56 -0400

    139: Removed user ability to update profile picture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,8 +43,7 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: 'HomePage' },
       { title: 'Directory', component: 'DirectoryPage' },
-      { title: 'Faceoff', component: 'FaceoffPage' },
-      { title: 'Profile Photo', component: 'PictureUploadPage' }
+      { title: 'Faceoff', component: 'FaceoffPage' }
     ];
   }
 
